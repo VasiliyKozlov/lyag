c436b506e9ed9a7f8ce5ef9e4a7a9d27a4f4b048
va.kozlov <vasdram@gmail.com>
2019-07-28 20:29:37 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import '../styles/styles.scss';
+//import '../../node_modules/flickity/dist/flickity.min.css';
 import activateComponent from './handlers/activateComponent';
 import activateOverflow from './handlers/activateOverflow';
-
+import Flickity from 'flickity';
 
 const menu = document.querySelector('.menu');
 const menuOpen = document.querySelector('.header__menu-btn');
 const menuClose = document.querySelector('.menu__header-close');
+var seviceSlider = document.querySelector('.services__slider');
+var promoSlider = document.querySelector('.promo__slider');
 //const menuClose = document.querySelector('.menu__header-close');
 
 menuOpen.addEventListener('click', e => {
@@ -16,4 +19,17 @@ menuOpen.addEventListener('click', e => {
 menuClose.addEventListener('click', e => {
   activateComponent(menu);
   activateOverflow();
+});
+
+var flkty = new Flickity(seviceSlider, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+var flktyPromo = new Flickity(promoSlider, {
+  // options
+  //cellAlign: 'center',
+  wrapAround: true
+  //contain: true
 });

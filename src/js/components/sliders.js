598e5ad125e8c267eb96d881bc8d3f6219fff955
va.kozlov <vasdram@gmail.com>
2019-07-28 21:00:14 +0300

    fix

---
+++ src/js/components/sliders.js
@@ -0,0 +1,17 @@
+import Flickity from 'flickity';
+
+var seviceSlider = document.querySelector('.services__slider');
+var promoSlider = document.querySelector('.promo__slider');
+
+var flkty = new Flickity(seviceSlider, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+var flktyPromo = new Flickity(promoSlider, {
+  // options
+  //cellAlign: 'center',
+  wrapAround: true
+  //contain: true
+});

d33699fe29db599a806e8de5d1a9035e965042b4
vasdram <vasdram@gmail.com>
2019-08-01 00:11:17 +0300

    fix

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,23 +1,23 @@
 import Flickity from 'flickity';
 
-var seviceSlider = document.querySelector('.services__slider');
-var promoSlider = document.querySelector('.promo__slider');
-var reviewsSlider = document.querySelector('.reviews__slider');
+const seviceSlider = document.querySelector('.services__slider');
+const promoSlider = document.querySelector('.promo__slider');
+const reviewsSlider = document.querySelector('.reviews__slider');
 
-var flkty = new Flickity(seviceSlider, {
+const flkty = new Flickity(seviceSlider, {
   // options
   cellAlign: 'left',
   contain: true
 });
 
-var flktyPromo = new Flickity(promoSlider, {
+const flktyPromo = new Flickity(promoSlider, {
   // options
   //cellAlign: 'center',
   wrapAround: true
   //contain: true
 });
 
-var flktyPromo = new Flickity(reviewsSlider, {
+const flktyReview = new Flickity(reviewsSlider, {
   cellAlign: 'left',
   contain: true
 });
